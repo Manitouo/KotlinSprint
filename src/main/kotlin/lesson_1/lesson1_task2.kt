12f25b9166e16f786568611c6da019e084c2696d
Alisha <verngrovalisha@yandex.ru>
2025-04-26 00:26:23 +0300

    KS-1-2

---
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -0,0 +1,16 @@
+package org.example.lesson_1
+
+fun main() {
+    var orderCount: Int = 75
+    val thankMessage: String = "Спасибо, что выбрали нас!"
+    var staffCount: Int = 2000
+
+    println(orderCount)
+    println(thankMessage)
+
+    //println(staffCount)
+
+    staffCount--
+    println(staffCount)
+
+}
