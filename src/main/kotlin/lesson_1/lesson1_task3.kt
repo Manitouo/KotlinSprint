b41bbbe127ff9417b87c664b85c3ad4a90d5ee71
Alisha <verngrovalisha@yandex.ru>
2025-04-26 00:33:03 +0300

    KS-1-3

---
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -0,0 +1,17 @@
+package org.example.lesson_1
+
+
+fun main() {
+    val year: String = "1961"
+    var hour: String = "09"
+    var minute: String = "07"
+
+    println("$year год")
+    println("$hour часов")
+    println("$minute минут")
+
+
+    hour = "10"
+    minute = "55"
+    print("\n$hour:$minute")
+}
