f4bcf0083955034bd20877ca4d67cbe2a141e5d0
Alisha <verngrovalisha@yandex.ru>
2025-04-27 12:20:34 +0300

    KS-1-2 deleted excess

--- src/main/kotlin/lesson_1/lesson1_task2.kt
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -3,11 +3,12 @@ package org.example.lesson_1
 fun main() {
     var orderCount: Int = 75
     val thankMessage: String = "Спасибо, что выбрали нас!"
-    var staffCount: Int = 2000
+
 
     println(orderCount)
     println(thankMessage)
 
+    var staffCount: Int = 2000
     //println(staffCount)
 
     staffCount--
