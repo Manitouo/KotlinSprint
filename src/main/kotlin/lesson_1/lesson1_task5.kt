2728e186840bd9b721f683b2ccdc2bcdae99d45b
Alisha <verngrovalisha@yandex.ru>
2025-04-27 14:12:06 +0300

    KS-1-5 add constant

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -2,10 +2,13 @@ package org.example.lesson_1
 
 fun main() {
     val seconds: Short = 6480
-    val hours: Int = seconds / 3600
-    val hoursRest: Int = seconds % 3600
-    val minutes: Int = hoursRest / 60
-    val sec: Int = seconds % 60
+    val twoStepsOfTime: Int = STEP_OF_TIME * STEP_OF_TIME
+    val hours: Int = seconds / twoStepsOfTime
+    val hoursRest: Int = seconds % twoStepsOfTime
+    val minutes: Int = hoursRest / STEP_OF_TIME
+    val sec: Int = seconds % STEP_OF_TIME
 
     println(String.format("%02d:%02d:%02d", hours, minutes, sec))
 }
+
+const val STEP_OF_TIME: Int = 60
