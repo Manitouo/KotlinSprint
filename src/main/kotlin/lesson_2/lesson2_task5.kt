365a63f2429cc5fa8ad1be7ddd3f54c8dc383408
Alisha <verngrovalisha@yandex.ru>
2025-04-28 12:02:48 +0300

    KS-2-5 second try

--- src/main/kotlin/lesson_2/lesson2_task5.kt
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -1,10 +1,10 @@
 package org.example.lesson_2
 
 fun main() {
-    val capitalStart: Int = 70000
-    val yearsOfCredit: Double = 20.0
+    val capitalStart = 70000
+    val yearsOfCredit = 20
 
-    val capitalEnd: Double = capitalStart * Math.pow((1 + PERCENTAGE_RATE), yearsOfCredit)
+    val capitalEnd: Double = capitalStart * Math.pow((1 + PERCENTAGE_RATE), yearsOfCredit.toDouble())
 
     println(String.format("%.3f - размер вклада через 20 лет", capitalEnd))
 }
