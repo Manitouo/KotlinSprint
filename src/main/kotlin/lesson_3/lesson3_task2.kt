e0b32623e0b9bb22366a495ccb460d61d4e29de1
Alisha <verngrovalisha@yandex.ru>
2025-05-01 16:42:11 +0300

    KS-3-2

---
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -0,0 +1,15 @@
+package org.example.lesson_3
+
+fun main() {
+    var age = 20
+    var surname = "Андреева"
+    var name = "Татьяна"
+    var patronymic = "Сергеевна"
+
+    println("$surname $name $patronymic, $age лет")
+
+    age = 22
+    surname = "Сидорова"
+
+    println("$surname $name $patronymic, $age года")
+}
