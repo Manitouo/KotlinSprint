d8ec1d1b186f4ba5204a79779c23e21a36e9bae3
Alisha <verngrovalisha@yandex.ru>
2025-05-01 11:44:09 +0300

    KS-2-5 update

--- src/main/kotlin/lesson_2/lesson2_task5.kt
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -4,8 +4,9 @@ fun main() {
     val capitalStart = 70000
     val yearsOfCredit = 20
 
-    val capitalEnd: Double = capitalStart * Math.pow((1 + PERCENTAGE_RATE), yearsOfCredit.toDouble())
+    val percentageShare = PERCENTAGE_RATE.toDouble() / 100
+    val capitalEnd: Double = capitalStart * Math.pow((1 + percentageShare), yearsOfCredit.toDouble())
 
     println(String.format("%.3f - размер вклада через 20 лет", capitalEnd))
 }
-const val PERCENTAGE_RATE = 0.167
+const val PERCENTAGE_RATE = 16.7
