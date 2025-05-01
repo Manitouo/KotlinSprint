683f986c009616e6283c36a55f9450bb5351c0af
Alisha <verngrovalisha@yandex.ru>
2025-05-01 11:42:14 +0300

    KS-2-4 update

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -4,12 +4,13 @@ fun main() {
     val crystalMineral: Int = 7
     val ironMineral: Int = 11
 
-    val benefitCrystal = crystalMineral * STRATEGY_BUFF
-    val benefitIron = ironMineral * STRATEGY_BUFF
+    val percentageShare = STRATEGY_BUFF.toDouble() / 100
+    val benefitCrystal = crystalMineral * percentageShare
+    val benefitIron = ironMineral * percentageShare
 
     println(String.format("%.0f - кристаллической руды \"сверху\"", benefitCrystal))
     println(String.format("%.0f - железной руды \"сверху\"", benefitIron))
 
 }
 
-const val STRATEGY_BUFF = 0.2
+const val STRATEGY_BUFF = 20
