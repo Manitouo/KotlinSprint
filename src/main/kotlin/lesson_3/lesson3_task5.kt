c66028dde52d135e32574d22f2c4933c2c65d943
Alisha <verngrovalisha@yandex.ru>
2025-05-02 11:55:02 +0300

    KS-3-5

---
+++ src/main/kotlin/lesson_3/lesson3_task5.kt
@@ -0,0 +1,14 @@
+package org.example.lesson_3
+
+fun main() {
+    val string = "E2-E4;0"
+
+    val separatedString = string.split('-', ';')
+
+    //Есть ли способ присвоить значения переменным, не используя массивы?
+    val value1 = separatedString[0]
+    val value2 = separatedString[1]
+    val value3 = separatedString[2]
+
+    print("$value1\n$value2\n$value3")
+}
