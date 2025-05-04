2259807712c3a8d9903fe9b450d863846f213222
Alisha <verngrovalisha@yandex.ru>
2025-05-04 18:27:47 +0300

    KS-4-2 update

--- src/main/kotlin/lesson_4/lesson4_task2.kt
+++ src/main/kotlin/lesson_4/lesson4_task2.kt
@@ -7,11 +7,15 @@ fun main() {
     val averageWeightSecond = 50
     val averageVolumeSecond = 100
 
-    println("Груз с весом ${averageWeightFirst}кг и объемом ${averageVolumeFirst}л соответствует категории 'Average': ${averageWeightFirst > AVERAGE_WEIGHT_MIN && averageWeightFirst <= AVERAGE_WEIGHT_MAX && averageVolumeFirst < AVERAGE_VOLUME}")
-    println("Груз с весом ${averageWeightSecond}кг и объемом ${averageVolumeSecond}л соответствует категории 'Average': ${averageWeightSecond > AVERAGE_WEIGHT_MIN && averageWeightSecond <= AVERAGE_WEIGHT_MAX && averageVolumeSecond < AVERAGE_VOLUME}")
+    println("Груз весом ${averageWeightFirst}кг и объемом ${averageVolumeFirst}л соответствует категории 'Average':" +
+            " ${averageWeightFirst > AVERAGE_WEIGHT_MIN && averageWeightFirst <= AVERAGE_WEIGHT_MAX && 
+                    averageVolumeFirst < AVERAGE_VOLUME_MAX}")
+    println("Груз весом ${averageWeightSecond}кг и объемом ${averageVolumeSecond}л соответствует категории 'Average':" +
+            " ${averageWeightSecond > AVERAGE_WEIGHT_MIN && averageWeightSecond <= AVERAGE_WEIGHT_MAX &&
+                    averageVolumeSecond < AVERAGE_VOLUME_MAX}")
 
 }
 
 const val AVERAGE_WEIGHT_MIN = 35
 const val AVERAGE_WEIGHT_MAX = 100
-const val AVERAGE_VOLUME = 100
+const val AVERAGE_VOLUME_MAX = 100
