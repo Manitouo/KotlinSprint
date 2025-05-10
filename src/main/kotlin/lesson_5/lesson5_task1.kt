248c7d5a9097f74ff643de12f41c4575204bac67
Alisha <lungoalina@gmail.com>
2025-05-10 18:37:44 +0300

    KS-5-1 update

--- src/main/kotlin/lesson_5/lesson5_task1.kt
+++ src/main/kotlin/lesson_5/lesson5_task1.kt
@@ -1,9 +1,11 @@
 package org.example.lesson_5
 
 fun main() {
-    print("Сколько будет 2 + 2? Введите ответ: ")
+    val number1 = 2
+    val number2 = 2
+    print("Сколько будет $number1 + $number2? Введите ответ: ")
     val answer = readln()
 
-    if (answer.toInt() == 4) println("Добро пожаловать!")
+    if (answer.toInt() == (number1 + number2)) println("Добро пожаловать!")
     else println("Доступ запрещен.")
 }
