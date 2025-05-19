f1a623cb544e6200357875e1e1a58bd52ebdc99e
Manitouo <157484247+Manitouo@users.noreply.github.com>
2025-05-19 20:46:25 +0300

    KS-9-4 update Array to List (sorted())

--- src/main/kotlin/lesson_9/lesson9_task4.kt
+++ src/main/kotlin/lesson_9/lesson9_task4.kt
@@ -2,13 +2,10 @@ package org.example.lesson_9
 
 fun main() {
     println("Перечислите 5 ингредиентов через запятую")
-    val ingredientsList = mutableListOf<String>()
-    val ingredients = readln().split(", ")
+
+    val ingredients = readln().split(", ").sorted()
+
     ingredients.forEach {
-        ingredientsList.add(it)
-    }
-    ingredientsList.sort()
-    ingredientsList.forEach {
         println(it)
     }
 }
