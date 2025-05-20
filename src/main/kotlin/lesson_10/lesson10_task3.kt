6301f0841c6bc6610352e7b4a00717d2f37bcafe
Manitouo <157484247+Manitouo@users.noreply.github.com>
2025-05-20 11:33:18 +0300

    KS-10-3

---
+++ src/main/kotlin/lesson_10/lesson10_task3.kt
@@ -0,0 +1,17 @@
+package org.example.lesson_10
+
+fun main() {
+    println("Введите длину пароля: ")
+    print(passwordGeneration(readln().toInt()))
+}
+
+fun passwordGeneration(length: Int) : String {
+    var password = ""
+    val numbersRange = 0..9
+    val symbolsRange = "!\"#$%&'()*+,-./ "
+    for (i in 1..length){
+        if (i % 2 == 0) password += symbolsRange.random()
+        else password += numbersRange.random()
+    }
+    return password
+}
