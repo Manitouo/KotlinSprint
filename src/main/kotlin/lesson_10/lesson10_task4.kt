7357c52e23ec44f0fc7976953657a596dac2b9b7
Manitouo <157484247+Manitouo@users.noreply.github.com>
2025-05-20 20:17:24 +0300

    KS-10-4 update counting victories

--- src/main/kotlin/lesson_10/lesson10_task4.kt
+++ src/main/kotlin/lesson_10/lesson10_task4.kt
@@ -5,7 +5,7 @@ import kotlin.random.Random
 fun main() {
     var victories = 0
     while (true) {
-        round(victories)
+        victories += conductRound()
         print("Хотите бросить кости еще раз? Введите Да или Нет: ")
         if (readln().equals("Нет", ignoreCase = true)) {
             println("Количество ваших побед: $victories")
@@ -14,20 +14,28 @@ fun main() {
     }
 }
 
-fun dice(): Int = Random.nextInt(1, 6)
+fun throwDice(): Int = Random.nextInt(1, 6)
 
-fun round(victories: Int): Int {
-    val computerTurn = dice()
+fun conductRound(): Int {
+    val computerTurn = throwDice()
     println("Очередь машины... Результат: $computerTurn")
-    val humanTurn = dice()
+    val humanTurn = throwDice()
     println("Ваша очередь... Результат: $humanTurn")
     when {
         computerTurn < humanTurn -> {
             println("Победило человечество")
-            victories++
+            return 1
         }
-        computerTurn == humanTurn -> println("Победила дружба")
-        else -> println("Победила машина")
+
+        computerTurn == humanTurn -> {
+            println("Победила дружба")
+            return 0
+        }
+
+        else -> {
+            println("Победила машина")
+            return 0
+        }
     }
-    return victories
+
 }
