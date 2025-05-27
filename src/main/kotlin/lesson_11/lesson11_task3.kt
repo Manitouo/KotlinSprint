35958428d26ed4a7ad24eaee8706f091b6630288
Manitouo <157484247+Manitouo@users.noreply.github.com>
2025-05-27 15:14:05 +0300

    KS-11-3

--- src/main/kotlin/lesson_11/lesson11_task3.kt
+++ src/main/kotlin/lesson_11/lesson11_task3.kt
@@ -4,38 +4,49 @@ class User3(
     val id: Int,
     val nickname: String,
     var picture: String,
-    var status: String,
-) {
-    fun updateStatus(statusNew: String) {
-        status = statusNew
-    }
-}
+    var status: String = "микрофон выключен",
+)
 
 class Room(
     val id: Int,
     var picture: String = "Обложка",
     var name: String,
-    var listOfMembers: Any? = null,
+    var listOfMembers: List<User3>,
 ) {
-    fun addUser(user: User3){
-        listOfMembers = user
+    fun addUser(user: User3) {
+        listOfMembers += user
+    }
+
+    fun updateStatus(name: String, statusNew: String) {
+        if (name == User3.nickname) User3.status = statusNew
     }
 }
 
 fun main() {
-    val user = User3 (
+    val user1 = User3(
         id = 4,
         nickname = "Alex",
         picture = "Аватар Александра",
         status = "микрофон выключен"
     )
+    val user2 = User3(
+        id = 5,
+        nickname = "Sophie",
+        picture = "Аватар Софьи",
+        status = "микрофон заглушен"
+    )
+
     val room = Room(
         id = 5,
         name = "Комната 1",
+        listOfMembers = listOf()
     )
 
-    user.updateStatus("пользователь заглушен")
-    println(user.status)
-    room.addUser(user)
+    room.addUser(user1)
+    room.addUser(user2)
+
+    room.updateStatus("Sophie", "микрофон включен")
+
+    println(user2.status)
     println(room.listOfMembers)
 }
