f879d98ed19171304f4fd628461d3773eba4d0a6
Manitouo <157484247+Manitouo@users.noreply.github.com>
2025-06-14 16:23:57 +0300

    KS-15-4

--- src/main/kotlin/lesson_15/lesson15_task4.kt
+++ src/main/kotlin/lesson_15/lesson15_task4.kt
@@ -4,21 +4,21 @@ interface Searching {
     fun searchProduct()
 }
 
-abstract class Product : Searching {
+abstract class Product {
     abstract val category: String
     abstract val name: String
     abstract val count: Int
-
-    override fun searchProduct() {
-        println("Выполняется поиск")
-    }
 }
 
 class Instrument(
     override val category: String = "Инструменты",
     override val name: String,
     override val count: Int,
-) : Product()
+) : Product(), Searching {
+    override fun searchProduct() {
+        println("Выполняется поиск")
+    }
+}
 
 class Accessory(
     override val category: String = "Комплектующие",
@@ -38,5 +38,4 @@ fun main() {
     )
 
     instrument1.searchProduct()
-    accessory1.searchProduct()
 }
