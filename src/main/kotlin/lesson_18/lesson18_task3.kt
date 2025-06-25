73d5e365a48ef749227ab97f3dd8b3f71a44c359
Manitouo <157484247+Manitouo@users.noreply.github.com>
2025-06-25 16:48:00 +0300

    KS-18-3

---
+++ src/main/kotlin/lesson_18/lesson18_task3.kt
@@ -0,0 +1,67 @@
+package org.example.lesson_18
+
+open class Animal() {
+    open fun feeding() {}
+    open fun sleeping() {}
+}
+
+class Fox(
+    private val name: String,
+    private val meal: String = "ягоды",
+) : Animal() {
+    override fun feeding() {
+        println(String.format("%s -> ест %s", name, meal))
+    }
+
+    override fun sleeping() {
+        println(String.format("%s -> спит", name))
+    }
+}
+
+class Dog(
+    private val name: String,
+    private val meal: String = "кости",
+) : Animal() {
+    override fun feeding() {
+        println(String.format("%s -> ест %s", name, meal))
+    }
+
+    override fun sleeping() {
+        println(String.format("%s -> спит", name))
+    }
+}
+
+class Cat(
+    private val name: String,
+    private val meal: String = "рыбу",
+) : Animal() {
+    override fun feeding() {
+        println(String.format("%s -> ест %s", name, meal))
+    }
+
+    override fun sleeping() {
+        println(String.format("%s -> спит", name))
+    }
+}
+
+fun main() {
+    val animalList: MutableList<Animal> = mutableListOf()
+
+    val fox1: Animal = Fox(
+        name = "Алиса"
+    )
+    val dog1: Animal = Dog(
+        name = "Шарик"
+    )
+    val cat1: Animal = Cat(
+        name = "Муся"
+    )
+
+    animalList.add(fox1)
+    animalList.add(dog1)
+    animalList.add(cat1)
+
+    for (element in animalList) {
+        element.feeding()
+    }
+}
