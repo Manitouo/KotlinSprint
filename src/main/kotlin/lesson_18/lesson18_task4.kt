903550fca6f5215d578715cb5b9ceb2542f324d9
Manitouo <157484247+Manitouo@users.noreply.github.com>
2025-06-27 15:24:11 +0300

    KS-18-4

--- src/main/kotlin/lesson_18/lesson18_task4.kt
+++ src/main/kotlin/lesson_18/lesson18_task4.kt
@@ -2,36 +2,40 @@ package org.example.lesson_18
 
 import kotlin.math.pow
 
-class Box() {
+abstract class Box() {
+    open fun packageCalculation() {}
+}
 
-    fun packageCalculation(
-        width: Double,
-        height: Double,
-        depth: Double,
-    ) {
+class RectangularBox(
+    val width: Double,
+    val height: Double,
+    val depth: Double,
+) : Box() {
+    override fun packageCalculation() {
         println("Площадь поверхности прямоугольной посылки: " +
                 "%.2f".format(2 * (width * height + width * depth + height * depth))
         )
     }
+}
 
-    fun packageCalculation(
-        side: Double,
-    ) {
+class SquareBox(
+    val side: Double,
+) : Box() {
+    override fun packageCalculation() {
         println("Площадь поверхности прямоугольной посылки: ${"%.2f".format(6 * side.pow(2.0))}")
     }
-
 }
 
 fun main() {
-    val box = Box()
-
-    box.packageCalculation(
+    val rectangularBox: Box = RectangularBox(
         width = 23.1,
         height = 12.8,
         depth = 9.2,
     )
-
-    box.packageCalculation(
-        side = 12.4
+    val squareBox: Box = SquareBox(
+        side = 12.4,
     )
+
+    rectangularBox.packageCalculation()
+    squareBox.packageCalculation()
 }
