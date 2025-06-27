9721bccb01f7c4164001a317d35baa1d187336e4
Manitouo <157484247+Manitouo@users.noreply.github.com>
2025-06-27 15:09:45 +0300

    KS-18-3

--- src/main/kotlin/lesson_18/lesson18_task3.kt
+++ src/main/kotlin/lesson_18/lesson18_task3.kt
@@ -1,59 +1,48 @@
 package org.example.lesson_18
 
-open class Animal() {
-    open fun feeding() {}
-    open fun sleeping() {}
+abstract class Animal(
+    _name: String,
+) {
+    open val name: String = _name
+    open val meal: String = ""
+
+    fun feeding() {
+        println(String.format("%s -> ест %s", name, meal))
+    }
+
+    fun sleeping() {
+        println(String.format("%s -> спит", name))
+    }
 }
 
 class Fox(
-    private val name: String,
-    private val meal: String = "ягоды",
-) : Animal() {
-    override fun feeding() {
-        println(String.format("%s -> ест %s", name, meal))
-    }
-
-    override fun sleeping() {
-        println(String.format("%s -> спит", name))
-    }
+    _name: String,
+) : Animal(_name) {
+    override val meal: String = "ягоды"
 }
 
 class Dog(
-    private val name: String,
-    private val meal: String = "кости",
-) : Animal() {
-    override fun feeding() {
-        println(String.format("%s -> ест %s", name, meal))
-    }
-
-    override fun sleeping() {
-        println(String.format("%s -> спит", name))
-    }
+    _name: String,
+) : Animal(_name) {
+    override val meal: String = "кости"
 }
 
 class Cat(
-    private val name: String,
-    private val meal: String = "рыбу",
-) : Animal() {
-    override fun feeding() {
-        println(String.format("%s -> ест %s", name, meal))
-    }
-
-    override fun sleeping() {
-        println(String.format("%s -> спит", name))
-    }
+    _name: String,
+) : Animal(_name) {
+    override val meal: String = "рыбу"
 }
 
 fun main() {
     val animalList: MutableList<Animal> = mutableListOf()
     val fox1: Animal = Fox(
-        name = "Алиса"
+        _name = "Алиса",
     )
     val dog1: Animal = Dog(
-        name = "Шарик"
+        _name = "Шарик",
     )
     val cat1: Animal = Cat(
-        name = "Муся"
+        _name = "Муся",
     )
 
     animalList.add(fox1)
