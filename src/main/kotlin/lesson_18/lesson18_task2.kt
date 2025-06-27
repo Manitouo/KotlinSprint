ba5ffb8a9d496712a3008ac3d13a5fe9a506b4ce
Manitouo <157484247+Manitouo@users.noreply.github.com>
2025-06-27 14:59:25 +0300

    KS-18-2

--- src/main/kotlin/lesson_18/lesson18_task2.kt
+++ src/main/kotlin/lesson_18/lesson18_task2.kt
@@ -3,38 +3,28 @@ package org.example.lesson_18
 import kotlin.random.Random
 
 open class Dice() {
-    open fun tossDice() {}
-}
+    open val facesCount: Int = 0
 
-class FourthDice(
-    private val facesCount: Int = 4,
-) : Dice() {
-    override fun tossDice() {
-        val value = Random.nextInt(1, 4)
+    open fun tossDice() {
+        val value = Random.nextInt(1, facesCount)
         println("У кубика $facesCount граней. Выпало число: $value")
     }
 }
 
-class SixthDice(
-    private val facesCount: Int = 6,
-) : Dice() {
-    override fun tossDice() {
-        val value = Random.nextInt(1, 6)
-        println("У кубика $facesCount граней. Выпало число: $value")
-    }
+class FourthDice() : Dice() {
+    override val facesCount: Int = 4
 }
 
-class EighthDice(
-    private val facesCount: Int = 8,
-) : Dice() {
-    override fun tossDice() {
-        val value = Random.nextInt(1, 8)
-        println("У кубика $facesCount граней. Выпало число: $value")
-    }
+class SixthDice(): Dice() {
+    override val facesCount: Int = 6
+}
+
+class EighthDice(): Dice() {
+    override val facesCount: Int = 8
 }
 
 fun main() {
-    var dicesList: MutableList<Dice> = mutableListOf()
+    val dicesList: MutableList<Dice> = mutableListOf()
 
     val dice1: Dice = FourthDice()
     val dice2: Dice = SixthDice()
