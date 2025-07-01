5b79ef9843f5542190bd19cab7163b468308621a
Manitouo <157484247+Manitouo@users.noreply.github.com>
2025-07-01 18:55:26 +0300

    KS-19-1

--- src/main/kotlin/lesson_19/lesson19_task1.kt
+++ src/main/kotlin/lesson_19/lesson19_task1.kt
@@ -1,25 +1,14 @@
 package org.example.lesson_19
 
-enum class Fish {
-    GUPPY,
-    ANGELFISH,
-    GOLDFISH,
-    SIAMESE_FIGHTING_FISH,
-}
-
-fun printFishList() {
-    Fish.entries.forEach {
-        when(it) {
-            Fish.GUPPY -> println("Гуппи")
-            Fish.ANGELFISH -> println("Скалярий")
-            Fish.GOLDFISH -> println("Золотая рыбка")
-            Fish.SIAMESE_FIGHTING_FISH -> println("Петушок")
-        }
-    }
+enum class Fish(var nameFish: String) {
+    GUPPY("Гуппи"),
+    ANGELFISH("Скалярия"),
+    GOLDFISH("Золотая рыбка"),
+    SIAMESE_FIGHTING_FISH("Петушок"),
 }
 
 fun main() {
     println("Вам доступны следующие рыбки:")
 
-    printFishList()
+    println(Fish.entries)
 }
